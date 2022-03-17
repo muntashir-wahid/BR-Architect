5d9e3b956e87094a171ddb664d09ef264c6c43fb
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-03-17 16:31:07 +0600

    fixed some bugs in header and implemented responsive nav feature with clickable toggle button

--- script.js
+++ script.js
@@ -3,11 +3,61 @@
 // HTML Elements
 
 const navLinksEl = document.querySelectorAll(".nav_link");
-const navBarEl = document.querySelector('nav');
-const headerEl = document.querySelector('header');
+const navBarEl = document.querySelector("nav");
+const headerEl = document.querySelector("header");
+const [, navMenuEl, toggleBtnEl] = document.querySelectorAll(".nav-items");
 
-// Smooth scroll 
 
+// Responsive navbar
+const responsiveNav = function () {
+
+  // Functions
+  const toggleNav = function () {
+    navMenuEl.classList.toggle("show-nav");
+  };
+
+  const hideNavMenuOnEachClick = function (e) {
+    const navLinks = e.target.classList.contains("nav_link");
+
+    // Guard clause
+    if (!navLinks) return;
+
+    // Hide nav menu when click on a link
+    navMenuEl.classList.remove("show-nav");
+  };
+
+  // Event Listeners
+  toggleBtnEl.addEventListener("click", toggleNav);
+  navMenuEl.addEventListener("click", hideNavMenuOnEachClick);
+};
+responsiveNav();
+
+
+// Sticky nav
+const stickyNav = function () {
+
+  // Callback function for the Intersection observer api
+  const toggleStickyNav = function (entries) {
+    const [entry] = entries;
+    if (!entry.isIntersecting) {
+      navBarEl.classList.add("sticky-nav");
+    } else {
+      navBarEl.classList.remove("sticky-nav");
+    }
+  };
+
+  // Header Observer
+  const headerObserver = new IntersectionObserver(toggleStickyNav, {
+    root: null,
+    threshold: 0,
+  });
+
+  headerObserver.observe(headerEl);
+};
+stickyNav();
+
+
+// Smooth scroll
 navLinksEl.forEach((link) => {
   link.addEventListener("click", function (e) {
     e.preventDefault();
@@ -30,22 +80,3 @@ navLinksEl.forEach((link) => {
   });
 });
 
-
-// Sticky nav
-
-const stickyNav = function(entries) {
-  const [entry] = entries;
-  // console.log(entry);
-  if (!entry.isIntersecting) {
-    navBarEl.classList.add('sticky-nav');
-  } else {
-    navBarEl.classList.remove('sticky-nav');
-  };
-}
-
-const headerObserver = new IntersectionObserver(stickyNav, {
-  root: null, 
-  threshold: 0,
-});
-
-headerObserver.observe(headerEl);
